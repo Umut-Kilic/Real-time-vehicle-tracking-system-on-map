d5210f1c9673913df6f90af6342c5f43fe9bd381
Göksel Kınalı <carryvayne12211@gmail.com>
2022-03-02 21:03:36 +0300

    Message broker ve firebase eklendi.

---
+++ fire/firebase.py
@@ -0,0 +1,13 @@
+import firebase_admin 
+from firebase_admin import credentials ,firestore
+
+kimlik= credentials.Certificate('./key.json')
+app=firebase_admin.initialize_app(kimlik)
+db=firestore.client()
+document=db.collection("data").document("veri1")
+
+
+document.set({
+    
+"veriyazi":"x"
+})
---
+++ message-broker/car3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import pika
+import sys
+import time
+import csv
+import pandas as pd
+sayac=0
+
+
+#allCars.csv
+#carid - > 1
+
+col_Names=["date", "x", "y", "carid"]
+data= pd.read_csv("allCars.csv",names=col_Names)
+car1 = data[data["carid"] ==3]
+x,y = car1.x , car1.y
+index=500
+
+while True:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+    
+    message= "ID : 3 :"+ str(  x[500+sayac]) +" "+ str( y[500+sayac]  )
+    channel.basic_publish(exchange='logs', routing_key='123', body=message)
+    print(" [x] Sent %r" % message)
+    connection.close()
+    sayac=sayac+1
+    time.sleep(0.25)
