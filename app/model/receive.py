8944f1b86635409b12a8b4be6cc244e7ace8fbad
UmutKilic1 <umutkilic3737@gmail.com>
2022-03-05 19:15:25 +0300

    mesaj brokerdan gelen veri parçalandı tutuldu

--- app/model/receive.py
+++ app/model/receive.py
@@ -4,9 +4,6 @@ import pika
 import sqlite3
 
 
-
-
-
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
 channel = connection.channel()
 
@@ -20,8 +17,19 @@ channel.queue_bind(exchange='logs', queue=queue_name)
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
+    decoded_string = body.decode("utf-8")
+    listem=decoded_string.split()
+    idd=listem[0][3:]
+    x=listem[3]
+    y=listem[6]
+    date=listem[7][5:]+" "+listem[8]
+    print("listem id :"+idd)
+    print("listem x :"+x)
+    print("listem y :"+y)
+    print("listem date :"+date)
+
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 channel.start_consuming()
+
